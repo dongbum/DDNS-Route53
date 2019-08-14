4a0959b7e10ba9db783656b5b60575bf36c53b8b
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-14 11:55:58 +0900

    Add file read and write

--- update.py
+++ update.py
@@ -20,20 +20,23 @@ s.close()
 # print(r)
 
 html = urlopen("http://bot.whatismyipaddress.com")
-result = BeautifulSoup(html, "html.parser")
+current_ip = BeautifulSoup(html, "html.parser")
 
-print(str(result)) # 실제 아이피를 가져올 수 있다.
+print(str(current_ip)) # 실제 아이피를 가져올 수 있다.
 
 client = boto3.client('route53')
 
 try:
     if os.path.exists('old_ip.txt'):
         f = open('old_ip.txt', mode='rt', encoding='utf-8')
-        old_ip = f.read()
-        print('old_ip : ' + old_ip)
+        old_ip = f.read()   # 파일이 이미 존재하면 아이피를 읽어온다.
         f.close()
     else:
-        print('file not found')
+        f = open('old_ip.txt', mode='wt', encoding='utf-8')
+        f.write(str(current_ip))    # 파일이 존재하지 않는다면 파일을 생성하고 현재 아이피를 추가한다.
+        old_ip = str(current_ip)
+        f.close()
 
+    print('old_ip : ' + old_ip)
 except FileNotFoundError:
     print('file error')
