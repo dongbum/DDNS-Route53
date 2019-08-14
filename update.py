1938e756bd81048e4f978440b853641c355e6da7
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-15 01:39:07 +0900

    Add config file load exception

--- update.py
+++ update.py
@@ -3,16 +3,22 @@
 from ddns.iputils import IPUtils
 from ddns.recordfile import RecordFile
 from ddns.awscommand import AWSCommand
+from ddns.customerror import *
 
-current_ip = IPUtils.getrealip()
+try:
+    current_ip = IPUtils.getrealip()
 
-# old_ip = RecordFile.get_old_ip(current_ip)  # 예전 IP를 가져온다.
-old_ip = AWSCommand.get_ip()
+    # old_ip = RecordFile.get_old_ip(current_ip)  # 예전 IP를 가져온다.
+    old_ip = AWSCommand.get_ip()
 
-print('Old:[%s] Current:[%s]' % (old_ip, current_ip)) # 실제 아이피를 가져올 수 있다.
+    print('Old:[%s] Current:[%s]' % (old_ip, current_ip)) # 실제 아이피를 가져올 수 있다.
 
-if str(old_ip) == str(current_ip):
-    print('same')
-else:
-    print('not same')
-    AWSCommand.update_ip(current_ip)
+    if str(old_ip) == str(current_ip):
+        print('same')
+    else:
+        print('not same')
+        AWSCommand.update_ip(current_ip)
+
+except Exception as e:
+    print(e)
+    exit(-1)
