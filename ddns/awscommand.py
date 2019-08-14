6381de9cdfe23c69bc04a32160f279d4c90cd685
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-14 17:55:17 +0900

    Modify AWS command class

--- ddns/awscommand.py
+++ ddns/awscommand.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import boto3
+from ddns.config import Config
 
 class AWSCommand:
     @staticmethod
     def update_ip(domain, current_ip):
+        config = Config()
+        domain = config.get_domain()
+        aws_hosted_zone_id = config.get_aws_hosted_zone_id()
+
         client = boto3.client('route53')
         response = client.change_resource_record_sets(
-            HostedZoneId='string',
+            HostedZoneId=aws_hosted_zone_id,
             ChangeBatch={
                 'Comment': 'string',
                 'Changes': [
