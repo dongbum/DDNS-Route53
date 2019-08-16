9bad76d4255d42b719f8a751bda6506d6905d806
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-16 12:25:55 +0900

    Add log class

--- ddns/logmanager.py
+++ ddns/logmanager.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+
+import logging
+import logging.handlers
+
+class Log:
+    logger = logging.getLogger('crumbs')
+    logger.setLevel(logging.DEBUG)
+
+    def __init__(self):
+        formatter = logging.Formatter('[%(asctime)s|%(levelname)s|%(filename)s:%(lineno)s] %(message)s')
+        file_max_bytes = 10 * 1024 * 1024
+
+        file_handler = logging.handlers.RotatingFileHandler(filename='./log/test.log', maxBytes=file_max_bytes)
+        stream_handler = logging.StreamHandler()
+
+        file_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    @classmethod
+    def write(cls, message):
+        cls.logger.debug(message)
