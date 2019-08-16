78ed4e101223836638f9255372561a946e1dfb2c
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-16 16:47:52 +0900

    Add console log mode.

--- ddns/logmanager.py
+++ ddns/logmanager.py
@@ -14,21 +14,26 @@ class Log:
         config = Config()
         log_path = config.log
 
-        if str(log_path).upper() == 'NONE':
+        formatter = logging.Formatter('[%(asctime)s|%(levelname)s|%(filename)s:%(lineno)s] %(message)s')
+
+        if str(log_path).upper() == 'CONSOLE':
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            self.logger.addHandler(stream_handler)
+        elif str(log_path).upper() == 'NONE':
             is_log = False
             return
+        else:
+            file_max_bytes = 10 * 1024 * 1024
 
-        formatter = logging.Formatter('[%(asctime)s|%(levelname)s|%(filename)s:%(lineno)s] %(message)s')
-        file_max_bytes = 10 * 1024 * 1024
+            file_handler = logging.handlers.RotatingFileHandler(filename=log_path, maxBytes=file_max_bytes)
+            stream_handler = logging.StreamHandler()
 
-        file_handler = logging.handlers.RotatingFileHandler(filename=log_path, maxBytes=file_max_bytes)
-        stream_handler = logging.StreamHandler()
+            file_handler.setFormatter(formatter)
+            stream_handler.setFormatter(formatter)
 
-        file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
-
-        self.logger.addHandler(file_handler)
-        self.logger.addHandler(stream_handler)
+            self.logger.addHandler(file_handler)
+            self.logger.addHandler(stream_handler)
 
     @classmethod
     def write(cls, message):
