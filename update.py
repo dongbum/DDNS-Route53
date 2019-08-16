35ae2dcf84f380f1ed5088d8a0ab8ae60ecb6918
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-16 15:38:46 +0900

    Code fixed.

--- update.py
+++ update.py
@@ -4,21 +4,26 @@ from ddns.logmanager import Log
 from ddns.iputils import IPUtils
 from ddns.recordfile import RecordFile
 from ddns.awscommand import AWSCommand
-from ddns.customerror import *
+from ddns.config import Config
 
 try:
     log = Log()
     current_ip = IPUtils.getrealip()
 
-    # old_ip = RecordFile.get_old_ip(current_ip)  # 예전 IP를 가져온다.
-    old_ip = AWSCommand.get_ip()
+    config = Config()
+    if str(config.get_ip).upper() == 'FILE':
+        old_ip = RecordFile.get_old_ip(current_ip)  # 예전 IP를 가져온다.
+    elif str(config.get_ip).upper() == 'AWS':
+        old_ip = AWSCommand.get_ip()
+    else:
+        raise Exception('Cannot found IP get method.')
 
     Log.write('Old:[%s] Current:[%s]' % (old_ip, current_ip)) # 실제 아이피를 가져올 수 있다.
 
     if str(old_ip) == str(current_ip):
-        Log.write('same')
+        Log.write('IP not changed.')
     else:
-        Log.write('not same')
+        Log.write('IP change detected.')
         AWSCommand.update_ip(current_ip)
 
 except Exception as e:
